1f718e99218705cb36e931cfc6689fe778b2974a
Parsa R <94003454+Parsa-03@users.noreply.github.com>
2022-05-17 08:56:16 +0430

    Update CompoundEffect.c

--- CompoundEffect.c
+++ CompoundEffect.c
@@ -3,7 +3,7 @@ int main () //compound effect 1% per day
 {
     int a ; // days
     float b=100 ;
-    
+    printf("Hello there. I'm gonna calculate the compund effect on your life! please give me how many days you are going to use compund effect?\n");
     scanf("%i",&a);
     for(int i=0 ; i<a ; i++ ){
         b= (b*101)/100 ;
